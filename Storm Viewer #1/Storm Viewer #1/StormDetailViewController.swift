1964bd48a27ef558772b8e2f2f9840b69d2fc1a7
Berserk-21 <cryptogekkofr@protonmail.com>
2023-08-11 15:58:09 +0200

    add StormDetailViewController

---
+++ Storm Viewer #1/Storm Viewer #1/StormDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StormDetailViewController.swift
+//  Storm Viewer #1
+//
+//  Created by Berserk on 11/08/2023.
+//
+
+import UIKit
+
+class StormDetailViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
