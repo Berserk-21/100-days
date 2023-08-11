ee42fa4cc1342e2b18cbdfd2600d60b8df556e10
Berserk-21 <cryptogekkofr@protonmail.com>
2023-08-11 15:48:32 +0200

    Display titles and images in the cell

--- Storm Viewer #1/Storm Viewer #1/StormTableViewController.swift
+++ Storm Viewer #1/Storm Viewer #1/StormTableViewController.swift
@@ -32,5 +32,18 @@ class StormTableViewController: UITableViewController {
             }
         }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StormCell", for: indexPath)
+        
+        cell.imageView?.image = UIImage(named: images[indexPath.row])
+        cell.textLabel?.text = images[indexPath.row]
+        
+        return cell
+    }
 }
 
