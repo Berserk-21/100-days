3ca1f90d90723755b6110c7fb66ccda4ff2c50f3
Berserk-21 <cryptogekkofr@protonmail.com>
2023-08-14 13:59:58 +0200

    Setup Xcode project

---
+++ Guess The Flag/Guess The Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Berserk on 14/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
