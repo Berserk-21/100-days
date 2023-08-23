17220f248f9376bef60f36ecaf67837d7da23399
Berserk-21 <cryptogekkofr@protonmail.com>
2023-08-23 11:31:40 +0200

    Root VC becomes a FlagListTableViewController

--- Milestone challenge 1-3/Milestone challenge 1-3/FlagListTableViewController.swift
+++ Milestone challenge 1-3/Milestone challenge 1-3/FlagListTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FlagListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
