ed128cefc4c11ebc2c31a38213c09036aadc8150
Berserk-21 <cryptogekkofr@protonmail.com>
2023-08-23 11:57:30 +0200

    Add flags to the tableview

--- Milestone challenge 1-3/Milestone challenge 1-3/FlagListTableViewController.swift
+++ Milestone challenge 1-3/Milestone challenge 1-3/FlagListTableViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class FlagListTableViewController: UITableViewController {
+    
+    private var flagsImages: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loadFlags()
     }
 
-
+    private func loadFlags() {
+        
+        let fileManager = FileManager()
+        guard let path = Bundle.main.resourcePath else { return }
+        
+        let files = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        files.forEach { file in
+            if file.hasSuffix("png") {
+                flagsImages.append(file)
+            }
+        }
+        print(flagsImages)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "flagTableViewCell", for: indexPath)
+        
+        cell.imageView?.image = UIImage(named: flagsImages[indexPath.row])
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flagsImages.count
+    }
 }
 
