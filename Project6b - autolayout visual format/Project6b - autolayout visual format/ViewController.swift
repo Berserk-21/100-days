0f41ce3f8e068116deaceadafc63fe262b221cb9
Berserk-21 <cryptogekkofr@protonmail.com>
2023-09-19 16:29:27 +0200

    add project6b

---
+++ Project6b - autolayout visual format/Project6b - autolayout visual format/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b - autolayout visual format
+//
+//  Created by Berserk on 19/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
