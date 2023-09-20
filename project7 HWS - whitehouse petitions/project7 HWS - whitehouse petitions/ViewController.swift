39290d60f556da7ff56663d8fb815f457b072f57
Berserk-21 <cryptogekkofr@protonmail.com>
2023-09-20 15:31:10 +0200

    prepare cell data

--- project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
+++ project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
+    
+    private var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,15 @@ class MainTableViewController: UITableViewController {
         
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
 }
 
