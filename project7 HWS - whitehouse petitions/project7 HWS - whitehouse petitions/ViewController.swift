53931367d0d063489e51aecdaf485f7ac5621da8
Berserk-21 <cryptogekkofr@protonmail.com>
2023-09-20 15:27:59 +0200

    project7 initialised

---
+++ project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  project7 HWS - whitehouse petitions
+//
+//  Created by Berserk on 20/09/2023.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
