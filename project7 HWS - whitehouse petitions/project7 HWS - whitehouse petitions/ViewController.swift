c38f645de638ad502d4e0115c63cc01a2d43ce06
Berserk-21 <cryptogekkofr@protonmail.com>
2023-09-21 14:37:13 +0200

    present an alert for error and add a second controller

--- project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
+++ project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
@@ -17,24 +17,38 @@ class MainTableViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         guard let url = URL(string: urlString) else { return }
         
         if let data = try? Data(contentsOf: url) {
             self.parse(json: data)
+        } else {
+            showError()
         }
     }
     
     // MARK: - Methods
-    func parse(json: Data) {
+    private func parse(json: Data) {
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             tableView.reloadData()
         } else {
-            print("there was an error decoding")
+            showError()
         }
+    }
+    
+    private func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 
     // MARK: - UITableViewController DataSource
