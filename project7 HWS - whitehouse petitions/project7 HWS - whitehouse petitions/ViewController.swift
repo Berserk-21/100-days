84672cf9b55d4cce769a034b1630c2643169b71e
Berserk-21 <cryptogekkofr@protonmail.com>
2023-09-21 12:17:17 +0200

    parse and present data from an url

--- project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
+++ project7 HWS - whitehouse petitions/project7 HWS - whitehouse petitions/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
     
-    private var petitions = [String]()
+    private var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         
+        guard let url = URL(string: urlString) else { return }
+        
+        if let data = try? Data(contentsOf: url) {
+            self.parse(json: data)
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,9 +30,28 @@ class MainTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
         return cell
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        } else {
+            print("there was an error decoding")
+        }
     }
 }
 
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
