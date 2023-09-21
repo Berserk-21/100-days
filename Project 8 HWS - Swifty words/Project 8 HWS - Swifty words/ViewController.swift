879781b685d8d41fa24b3e3e394b6781c937b837
Berserk-21 <cryptogekkofr@protonmail.com>
2023-09-21 16:48:22 +0200

    initialize project8

---
+++ Project 8 HWS - Swifty words/Project 8 HWS - Swifty words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 8 HWS - Swifty words
+//
+//  Created by Berserk on 21/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
