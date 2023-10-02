0b2e535175e869969ad290c77b936efaf6d9f23b
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-02 15:25:30 +0200

    project initialized

---
+++ milestones challenge 7-9/milestones challenge 7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  milestones challenge 7-9
+//
+//  Created by Berserk on 02/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
