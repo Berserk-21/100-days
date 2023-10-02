0378450d6e446db851750364d61b6393be696c60
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-02 18:14:25 +0200

    add hangout drawing

--- milestones challenge 7-9/milestones challenge 7-9/ViewController.swift
+++ milestones challenge 7-9/milestones challenge 7-9/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var headView: UIView!
+    @IBOutlet private weak var leadingArmView: UIView!
+    @IBOutlet private weak var trailingArmView: UIView!
+    @IBOutlet private weak var leadingLegView: UIView!
+    @IBOutlet private weak var trailingLegView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupHangoutDrawing()
     }
 
+    private func setupHangoutDrawing() {
+        
+        headView.layer.cornerRadius = headView.frame.width / 2.0
+        headView.layer.masksToBounds = true
+        
+        leadingArmView.transform = leadingArmView.transform.rotated(by: .pi/4)
+        trailingArmView.transform = trailingArmView.transform.rotated(by: 3 * .pi/4)
+        leadingLegView.transform = leadingLegView.transform.rotated(by: -(.pi/4))
+        trailingLegView.transform = trailingLegView.transform.rotated(by: -(3 * .pi/4))
+    }
 
 }
 
