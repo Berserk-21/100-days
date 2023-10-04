3f0a1f3a47b2a4673b0ba3eff723ff30029549dd
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-04 12:10:21 +0200

    add PersonCollectionViewCell class

---
+++ Project10_collectionview/Project10_collectionview/PersonCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCollectionViewCell.swift
+//  Project10_collectionview
+//
+//  Created by Berserk on 04/10/2023.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
