ceba16bcd05e3f70c486a575ae4f9a46baf1b45d
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-04 12:07:27 +0200

    setup collectionview cell in storyboard

--- Project10_collectionview/Project10_collectionview/ViewController.swift
+++ Project10_collectionview/Project10_collectionview/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
