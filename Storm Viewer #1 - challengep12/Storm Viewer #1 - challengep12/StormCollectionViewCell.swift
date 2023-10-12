9c75378de35e9d80232aec9e89056738d243210f
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-12 15:41:44 +0200

    duplicate and add a saved views counter

---
+++ Storm Viewer #1 - challengep12/Storm Viewer #1 - challengep12/StormCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  StormCollectionViewCell.swift
+//  Storm Viewer #1
+//
+//  Created by Berserk on 06/10/2023.
+//
+
+import UIKit
+
+class StormCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "StormCollectionViewCell"
+    
+    @IBOutlet var stormImageView: UIImageView!
+}
