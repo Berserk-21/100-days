b0540d5afc1d24a2d21b8d7b06657491acee55d9
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-16 17:41:27 +0200

    add basic layout properties and methods

--- Project13_instafilter/Project13_instafilter/ViewController.swift
+++ Project13_instafilter/Project13_instafilter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var editedImageView: UIImageView!
+    @IBOutlet private weak var intensitySlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction private func changeFilter() {
+        
+    }
+    
+    @IBAction private func save() {
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        
+    }
 }
 
