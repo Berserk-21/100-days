0fddc63ba331084e835e1793216d4e7658d330f8
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-17 18:11:55 +0200

    can import photos from library and prepare buttons and method to edit them

--- Project13_instafilter/Project13_instafilter/ViewController.swift
+++ Project13_instafilter/Project13_instafilter/ViewController.swift
@@ -7,15 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    // MARK: - Properties
+
+    private var currentImage: UIImage?
+    private var originalImage: UIImage?
 
     @IBOutlet private weak var editedImageView: UIImageView!
     @IBOutlet private weak var intensitySlider: UISlider!
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setupLayout()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupLayout() {
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func importPicture() {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
     @IBAction private func changeFilter() {
@@ -28,6 +54,17 @@ class ViewController: UIViewController {
     
     @IBAction func intensityChanged(_ sender: Any) {
         
+    }
+    
+    // MARK: - UIImage Picker Delegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let editedImage = info[.editedImage] as? UIImage, let originalImage = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = editedImage
+        self.originalImage = originalImage
     }
 }
 
