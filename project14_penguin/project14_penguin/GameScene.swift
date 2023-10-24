ba9a028196f897de053f08c4868580aa0ef63d8b
Berserk-21 <cryptogekkofr@protonmail.com>
2023-10-24 15:54:40 +0200

    project14 initialized

---
+++ project14_penguin/project14_penguin/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  project14_penguin
+//
+//  Created by Berserk on 24/10/2023.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
