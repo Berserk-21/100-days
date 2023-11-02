b114530517ec142d9ace1458cce56af594c3bbff
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-02 15:16:26 +0100

    Initialized project

---
+++ Project 16 - Capital Cities/Project 16 - Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 16 - Capital Cities
+//
+//  Created by Berserk on 02/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
