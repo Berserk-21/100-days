033fcb2a5dae14c7992a8aa36d87487b12f4a676
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-02 11:57:09 +0100

    add image and tap button for iPad landscape

--- Project15 - Animation/Project15 - Animation/ViewController.swift
+++ Project15 - Animation/Project15 - Animation/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var imageView: UIImageView!
+    private var currentAnimation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin")) 
+        imageView.center = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+        view.addSubview(imageView)
     }
 
-
+    @IBAction private func didTap() {
+        
+    }
 }
 
