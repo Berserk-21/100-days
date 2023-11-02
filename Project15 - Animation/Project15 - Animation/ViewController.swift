05af2f2adfc74a396855f2113421c7cfdc68e12b
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-02 12:13:52 +0100

    add 7 different CGAffine transformations

--- Project15 - Animation/Project15 - Animation/ViewController.swift
+++ Project15 - Animation/Project15 - Animation/ViewController.swift
@@ -21,8 +21,42 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
     }
 
-    @IBAction private func didTap() {
+    @IBAction private func didTap(_ sender: UIButton) {
         
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: []) {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            case 1:
+                self.imageView.transform = CGAffineTransform.identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256.0, y: -256.0)
+            case 3:
+                self.imageView.transform = CGAffineTransform.identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            case 5:
+                self.imageView.transform = CGAffineTransform.identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.green
+            case 7:
+                self.imageView.alpha = 1.0
+                self.imageView.backgroundColor = UIColor.clear
+            default:
+                break
+            }
+        } completion: { finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
     }
 }
 
