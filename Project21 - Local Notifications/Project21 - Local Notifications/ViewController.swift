69c70694e6e5a95cb9ac2104ecc22dab7593cd2b
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-10 15:14:25 +0100

    project initialized

---
+++ Project21 - Local Notifications/Project21 - Local Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21 - Local Notifications
+//
+//  Created by Berserk on 10/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
