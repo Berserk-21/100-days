2d4e6f233f4679aaebfadd733f83c0ea941c9d5c
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-13 16:08:48 +0100

    project initialized

---
+++ Project22-Beacon/Project22-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22-Beacon
+//
+//  Created by Berserk on 13/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
