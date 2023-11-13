48e20ad5e6e5af9cf389ea13305ff400cdef1a3a
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-13 16:28:48 +0100

    request location authorization

--- Project22-Beacon/Project22-Beacon/ViewController.swift
+++ Project22-Beacon/Project22-Beacon/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // MARK: - Properties
+    
+    @IBOutlet private weak var distanceReadingLabel: UILabel!
+    var locationManager: CLLocationManager?
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
     }
-
     
+    // MARK: - CLLocationManager Delegate
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
+    }
 }
 
