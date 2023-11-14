1595296775f1b875ce14ab5650d895f44d0c502e
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-14 16:36:08 +0100

    remove prefix and suffix if characters are found

--- Project24-Strings.playground/Contents.swift
+++ Project24-Strings.playground/Contents.swift
@@ -4,6 +4,18 @@ extension String {
     subscript(i: Int) -> String {
         return String(self[index(startIndex, offsetBy: i)])
     }
+    
+    // remove a prefix if it exists
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    // remove a suffix if it exists
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
 }
 
 let name = "Taylor"
@@ -12,8 +24,15 @@ for letter in name {
     print("Here is: \(letter)")
 }
 
+// Subscript, find a character in a String with an index
 let fourthLetter = name[name.index(name.startIndex, offsetBy: 0)]
-
 name[4]
 
+// Remove certain characters in a String
+let password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("345")
+
+let newPwrd = password.deletingPrefix("123")
+let dropLastPwrd = password.deletingSuffix("45")
 
