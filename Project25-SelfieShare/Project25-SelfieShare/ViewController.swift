1097ab251119506f2d159898774c8bfe86d37edc
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-23 17:02:37 +0100

    remplace le view controller par un collectionVC avec sa cellule "imageView"

--- Project25-SelfieShare/Project25-SelfieShare/ViewController.swift
+++ Project25-SelfieShare/Project25-SelfieShare/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotosCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
