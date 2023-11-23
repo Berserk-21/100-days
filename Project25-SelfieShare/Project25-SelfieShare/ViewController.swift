4444383448bffc8ff2645a66a8ec928ae1795830
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-23 17:09:26 +0100

    add collection view data source methods and basic layout

--- Project25-SelfieShare/Project25-SelfieShare/ViewController.swift
+++ Project25-SelfieShare/Project25-SelfieShare/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    private var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func importPicture() {
+        
+    }
+    
+    // MARK: - CollectionView DataSource
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
 
-
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
 }
 
