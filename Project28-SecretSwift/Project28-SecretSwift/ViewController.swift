5d2a6748b65fdeb07dff5721dc2ffc00e194d7ee
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-30 11:13:29 +0100

    Initialized project

---
+++ Project28-SecretSwift/Project28-SecretSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project28-SecretSwift
+//
+//  Created by Berserk on 30/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
