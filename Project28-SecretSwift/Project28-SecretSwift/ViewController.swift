add3bcb3570f6c9032fab7c48e3960e6d7c8369a
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-30 12:18:27 +0100

    Ajout d'une textView et de son ajustement du contentInset en fonction des mouvements du clavier

--- Project28-SecretSwift/Project28-SecretSwift/ViewController.swift
+++ Project28-SecretSwift/Project28-SecretSwift/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var secretTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let notificationCenter = NotificationCenter.default
+        
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
-
+    
+    @objc private func adjustForKeyboard(notification: Notification) {
+        
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardRect = keyboardValue.cgRectValue
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secretTextView.contentInset = .zero
+        } else {
+            secretTextView.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardRect.height - view.safeAreaInsets.bottom, right: 0.0)
+        }
+        
+        secretTextView.scrollIndicatorInsets = secretTextView.contentInset
+        
+        let selectedRange = secretTextView.selectedRange
+        secretTextView.scrollRangeToVisible(selectedRange)
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        
+        return true
+    }
 
 }
 
