6ea15bac76e83b35453e675e6f66d9af8414aa48
Berserk-21 <cryptogekkofr@protonmail.com>
2023-12-13 15:02:59 +0100

    Ajout d'un textfield et d'une stack view via le storyboard

--- Project31-Multibrowser/Project31-Multibrowser/ViewController.swift
+++ Project31-Multibrowser/Project31-Multibrowser/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    @IBOutlet private weak var addressBar: UITextField!
+    @IBOutlet private weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
